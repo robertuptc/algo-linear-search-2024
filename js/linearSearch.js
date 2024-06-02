06da77d4affd6d9cc1f944dc206afca06308b5ec
robertuptc <robertuptc@gmail.com>
2024-06-02 19:39:06 -0400

    js problem finished

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -4,6 +4,30 @@ for (let i = 1; i <= 1000; i++) {
 }
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
+    // Release 0
+    let answer = ''
 
+    for (let i = 0; i < arrayToSearchThrough.length; i ++) {
+        if (arrayToSearchThrough[i] == valueToFind) {
+            if (!answer) {
+                answer = i
+            }
+        }
+    }
+    if (answer) {
+        return answer
+    } else {
+        return undefined
+    }
 };
 
+exports.linearSearchGlobal = function(valueToFind, arrayToSearchThrough) {
+    let answer = []
+
+    for (let i = 0; i < arrayToSearchThrough.length; i ++) {
+        if (arrayToSearchThrough[i] == valueToFind) {
+            answer.push(i)
+        }
+    }
+    return answer
+}
