4d143bc3eb6da25fdb4384646a64169065ace22f
robertuptc <robertuptc@gmail.com>
2024-06-05 10:20:16 -0400

    python problem finished

--- python/linear_search.py
+++ python/linear_search.py
@@ -4,3 +4,14 @@ for number in range(1, 1001):
 
 def linear_search(value_to_find, array_to_search_through):
     # your code here
+    if value_to_find in array_to_search_through:
+        return array_to_search_through.index(value_to_find)
+
+
+def global_linear_search(value_to_find, array_to_search_through):
+    answer = []
+
+    for i in range(len(array_to_search_through)):
+        if value_to_find == array_to_search_through[i]:
+            answer.append(i)
+    return answer
--- python/linear_search_spec.py
+++ python/linear_search_spec.py
@@ -1,5 +1,7 @@
-from linear_search import linear_search
+from linear_search import linear_search, global_linear_search
 
 print(linear_search(3, [1,2,3]) == 2)
 print(linear_search(4, [1,2,3]) == None)
 print(linear_search(13, [1,2,3]) == None)
+
+print(global_linear_search("a", ["b", "a", "n", "a", "n", "a", "s"]) == [1, 3, 5])
